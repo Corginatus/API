1903cae9cdbc7f8de6441fe85c35be8d134cbf50
Corginatus <70595766+Corginatus@users.noreply.github.com>
2022-07-05 21:17:15 +0300

    api

--- index.mjs
+++ index.mjs
@@ -1,6 +1,6 @@
 import fetch from "node-fetch";
 
-fetch("https://176.196.250.86:3128/spd-classif.gate.petersburg.ru/api/v2/datasets/139/versions/latest/data/569/")
+fetch("https://spd-classif.gate.petersburg.ru/api/v2/datasets/139/versions/latest/data/569/")
 .then(res => {
     if(res.ok){
         return res.json()
